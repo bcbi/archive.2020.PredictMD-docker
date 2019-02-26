81667a572e8ac17beca1ebbc96c08912775a56ea
Dilum Aluthge <dilum@aluthge.com>
2019-02-25 22:45:59 -0500

    Progress commit

--- predictmd/predictmd-unstable/julia_scripts/external_pkgs.jl
+++
@@ -1,6 +0,0 @@
-println("Python")
-ENV["PYTHON"]=""
-const external_pkgs = ["PyCall", "PyPlot", "IJulia", "ScikitLearn", "Seaborn", "Pandas"]
-
-# "RCall" Error: R_HOME not a directory
-# "Seaborn" ERROR: UndefVarError: warn not defined
--- predictmd/predictmd-unstable/runtests.sh
+++ predictmd/predictmd-unstable/runtests.sh
@@ -8,9 +8,9 @@ export GROUP="all"
 export PREDICTMD_TEST_GROUP="all"
 export PREDICTMD_OPEN_PLOTS_DURING_TESTS="true"
 
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import PredictMD;'
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import PredictMDExtra;'
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import PredictMDFull;'
+julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; Pkg.activate("PredictMD-unstable"); import PredictMD;'
+julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; Pkg.activate("PredictMD-unstable"); import PredictMDExtra;'
+julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; Pkg.activate("PredictMD-unstable"); import PredictMDFull;'
 
 echo "predictmd-unstable: runtests.sh: Tests passed."
 
