17e464606cc8782fde4c45a31da7a4ec7f4f6f1f
Dilum Aluthge <dilum@aluthge.com>
2019-02-25 23:41:20 -0500

    Update sh_startup.jl

--- predictmd/predictmd-unstable/sh_startup.jl
+++ predictmd/predictmd-unstable/sh_startup.jl
@@ -1,7 +1,7 @@
-using Pkg
+import Pkg
 
 try
-    Pkg.Registry.add(RegistrySpec(name= "StrongHoldGeneral", path="/opt/julia/share/julia/registries/StrongHoldGeneral"));
+    Pkg.Registry.add(Pkg.RegistrySpec(name= "StrongHoldGeneral", path="/opt/julia/share/julia/registries/StrongHoldGeneral"));
 catch e
     @warn("ignoring exception: ", e,)
 end
