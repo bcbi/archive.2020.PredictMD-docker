176784eb287dab6c40cbba307eeadeae058c1ba1
Dilum Aluthge <dilum@aluthge.com>
2019-02-26 13:12:35 -0500

    Progress commit

--- predictmd/predictmd-unstable/runtests-all.sh
+++ predictmd/predictmd-unstable/runtests-all.sh
@@ -8,9 +8,27 @@ export GROUP="all"
 export PREDICTMD_TEST_GROUP="all"
 export PREDICTMD_OPEN_PLOTS_DURING_TESTS="true"
 
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; Pkg.test("PredictMD");'
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; Pkg.test("PredictMDExtra");'
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; Pkg.test("PredictMDFull");'
+julia -e '
+    ENV["JULIA_DEBUG"] = "all";
+    pushfirst!(DEPOT_PATH,"/opt/julia/share/julia");
+    pushfirst!(DEPOT_PATH,joinpath(homedir(),".julia",),);
+    import Pkg;
+    Pkg.test("PredictMD");
+    '
+julia -e '
+    ENV["JULIA_DEBUG"] = "all";
+    pushfirst!(DEPOT_PATH,"/opt/julia/share/julia");
+    pushfirst!(DEPOT_PATH,joinpath(homedir(),".julia",),);
+    import Pkg;
+    Pkg.test("PredictMDExtra");
+    '
+julia -e '
+    ENV["JULIA_DEBUG"] = "all";
+    pushfirst!(DEPOT_PATH,"/opt/julia/share/julia");
+    pushfirst!(DEPOT_PATH,joinpath(homedir(),".julia",),);
+    import Pkg;
+    Pkg.test("PredictMDFull");
+    '
 
 echo "predictmd-unstable: runtests-all.sh: Tests passed."
 
--- predictmd/predictmd-unstable/runtests.sh
+++ predictmd/predictmd-unstable/runtests.sh
@@ -8,9 +8,24 @@ export GROUP="all"
 export PREDICTMD_TEST_GROUP="all"
 export PREDICTMD_OPEN_PLOTS_DURING_TESTS="true"
 
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import PredictMD;'
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import PredictMDExtra;'
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import PredictMDFull;'
+julia -e '
+    ENV["JULIA_DEBUG"] = "all";
+    pushfirst!(DEPOT_PATH,"/opt/julia/share/julia");
+    pushfirst!(DEPOT_PATH,joinpath(homedir(),".julia",),);
+    import PredictMD;
+    '
+julia -e '
+    ENV["JULIA_DEBUG"] = "all";
+    pushfirst!(DEPOT_PATH,"/opt/julia/share/julia");
+    pushfirst!(DEPOT_PATH,joinpath(homedir(),".julia",),);
+    import PredictMDExtra;
+    '
+julia -e '
+    ENV["JULIA_DEBUG"] = "all";
+    pushfirst!(DEPOT_PATH,"/opt/julia/share/julia");
+    pushfirst!(DEPOT_PATH,joinpath(homedir(),".julia",),);
+    import PredictMDFull;
+    '
 
 echo "predictmd-unstable: runtests.sh: Tests passed."
 
