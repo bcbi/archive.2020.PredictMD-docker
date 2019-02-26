58d58d886bd7082a5c91df7766a3fea67e790bcd
Dilum Aluthge <dilum@aluthge.com>
2019-02-25 23:37:30 -0500

    Progress commit

--- predictmd/predictmd-unstable/runtests-all.sh
+++ predictmd/predictmd-unstable/runtests-all.sh
@@ -8,9 +8,9 @@ export GROUP="all"
 export PREDICTMD_TEST_GROUP="all"
 export PREDICTMD_OPEN_PLOTS_DURING_TESTS="true"
 
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; Pkg.activate("PredictMD-unstable"); Pkg.test("PredictMD");'
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; Pkg.activate("PredictMD-unstable");Pkg.test("PredictMDExtra");'
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; Pkg.activate("PredictMD-unstable");Pkg.test("PredictMDFull");'
+julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; Pkg.test("PredictMD");'
+julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; Pkg.test("PredictMDExtra");'
+julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; Pkg.test("PredictMDFull");'
 
 echo "predictmd-unstable: runtests-all.sh: Tests passed."
 
