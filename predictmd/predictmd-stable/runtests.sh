aca843a89e8f0599a72bfba75b4572c391866d9c
Dilum Aluthge <dilum@aluthge.com>
2019-02-25 23:40:53 -0500

    Update runtests.sh

--- predictmd/predictmd-stable/runtests.sh
+++ predictmd/predictmd-stable/runtests.sh
@@ -8,9 +8,9 @@ export GROUP="all"
 export PREDICTMD_TEST_GROUP="all"
 export PREDICTMD_OPEN_PLOTS_DURING_TESTS="true"
 
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; import PredictMD;'
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; import PredictMDExtra;'
-julia -e 'ENV["JULIA_DEBUG"] = "all"; import Pkg; import PredictMDFull;'
+julia -e 'ENV["JULIA_DEBUG"] = "all"; import PredictMD;'
+julia -e 'ENV["JULIA_DEBUG"] = "all"; import PredictMDExtra;'
+julia -e 'ENV["JULIA_DEBUG"] = "all"; import PredictMDFull;'
 
 echo "predictmd-stable: runtests.sh: Tests passed."
 
