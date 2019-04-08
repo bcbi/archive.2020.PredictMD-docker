d728977c7b2cfcbf72642777d3d3ef7cfef6e612
Dilum Aluthge <dilum@aluthge.com>
2019-04-07 20:35:17 -0400

    Push to Docker Hub when building the staging branch

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -20,7 +20,6 @@ make build
 make test-all
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]
-then
     if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]
     then
         echo "$DOCKER_BOT_PASSWORD" | docker login -u "$DOCKER_BOT_USERNAME" --password-stdin
@@ -28,8 +27,20 @@ then
     else
         :
     fi
+then
 else
-    :
+    if [[ "$TRAVIS_BRANCH" == "staging" ]]
+    then
+        if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]
+        then
+            echo "$DOCKER_BOT_PASSWORD" | docker login -u "$DOCKER_BOT_USERNAME" --password-stdin
+            make push
+        else
+            :
+        fi
+    else
+        :
+    fi
 fi
 
 pwd
