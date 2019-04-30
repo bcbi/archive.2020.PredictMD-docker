418c8179aae858b96cec2bf1a55ee4a5fe4a16db
Dilum Aluthge <dilum@aluthge.com>
2019-04-29 23:26:47 -0400

    Push to different images based on the branch

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -4,6 +4,25 @@
 
 set -ev
 
+if [[ "$TRAVIS_BRANCH" == "master" ]]
+then
+    export IMAGE_NAME_PREFIX=""
+else
+    if [[ "$TRAVIS_BRANCH" == "staging" ]]
+    then
+        export IMAGE_NAME_PREFIX="staging-"
+    else
+        if [[ "$TRAVIS_BRANCH" == "trying" ]]
+        then
+            export IMAGE_NAME_PREFIX="trying-"
+        else
+            export IMAGE_NAME_PREFIX="TRAVIS-DO-NOT-PUSH-"
+        fi
+    fi
+fi
+
+echo "IMAGE_NAME_PREFIX=$IMAGE_NAME_PREFIX"
+
 pwd
 cd $TRAVIS_BUILD_DIR
 pwd
@@ -19,28 +38,29 @@ pwd
 make build
 make test-all
 
-if [[ "$TRAVIS_BRANCH" == "master" ]]
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]
 then
-    if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]
+    if [[ "$TRAVIS_BRANCH" == "master" ]]
     then
         echo "$DOCKER_BOT_PASSWORD" | docker login -u "$DOCKER_BOT_USERNAME" --password-stdin
         make push
     else
-        :
-    fi
-else
-    if [[ "$TRAVIS_BRANCH" == "staging" ]]
-    then
-        if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]
+        if [[ "$TRAVIS_BRANCH" == "staging" ]]
         then
             echo "$DOCKER_BOT_PASSWORD" | docker login -u "$DOCKER_BOT_USERNAME" --password-stdin
             make push
         else
-            :
+            if [[ "$TRAVIS_BRANCH" == "trying" ]]
+            then
+                echo "$DOCKER_BOT_PASSWORD" | docker login -u "$DOCKER_BOT_USERNAME" --password-stdin
+                make push
+            else
+                :
+            fi
         fi
-    else
-        :
     fi
+else
+    :
 fi
 
 pwd
