580ecd1aaa9ff46cb50e33ce24df5c796dbcf0fe
Dilum Aluthge <dilum@aluthge.com>
2019-07-16 03:10:42 -0400

    Update before_install.sh

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -7,6 +7,7 @@ cd $HOME
 export CURL_USER_AGENT="Travis-CI $(curl --version | head -n 1)"
 mkdir -p ~/julia
 curl -A "$CURL_USER_AGENT" -s -L --retry 7 "$JULIA_URL" | tar -C ~/julia -x -z --strip-components=1 -f -
+
 export PATH="${PATH}:${TRAVIS_HOME}/julia/bin"
 
 julia --color=yes -e "VERSION >= v\"0.7.0-DEV.3630\" && import InteractiveUtils; InteractiveUtils.versioninfo()"
